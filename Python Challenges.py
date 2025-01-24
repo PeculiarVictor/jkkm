962a2c78fff7ec679c4838e1cfa740bbc7018422
PeculiarVictor <160975802+PeculiarVictor@users.noreply.github.com>
2025-01-24 18:43:55 +0000

    Add files via upload

---
+++ Python Challenges.py
@@ -0,0 +1,15 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w) * x / y;
+print("Value of (v+w) * x/ y is " ,  z)
+name = "Rhema"
+age = 10
+
+
+if name == "Rhema" or name == "John" and age >=2 :
+   print ("hello! Welcome.")    
+else :
+    print("Good bye! !")
